96d3ca8877c24da1e67619ac8f41b822220df20a
colinkirby <ckirby1@macalester.edu>
2020-11-20 14:36:12 -0600

    Colin workspace & scripts
    Added simple removal of painting on wall

---
+++ Assets/Scripts/SelectionManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionManager : MonoBehaviour
+{
+    // Update is called once per frame
+
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private string selectableTag = "Selectable";
+
+    
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width/2f, Screen.height/2f, 0f));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            var selection = hit.transform;
+            print(this.gameObject);
+            
+            if(selection.CompareTag(selectableTag)) {
+                print("Looking at art");
+                if(Input.GetKeyDown (KeyCode.E)) {
+                    // var selectionRenderer = selection.GetComponent<Renderer>();
+                    // if (selectionRenderer != null) {
+                    //     selectionRenderer.material = highlightMaterial;
+                    // }
+                    Destroy(gameObject);
+                }
+
+            } else {
+                print("Not looking at art");
+            }
+            
+        }
+
+    }
+}
