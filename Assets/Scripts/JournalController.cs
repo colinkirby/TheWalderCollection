2fd4052fdb2b217cdb10aa90be645670be7fc389
colinkirby <ckirby1@macalester.edu>
2020-12-08 14:54:03 -0600

    Finished journal and edited flashlight a bit

--- Assets/Scripts/JournalController.cs
+++ Assets/Scripts/JournalController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JournalController : MonoBehaviour
 {
@@ -10,10 +11,40 @@ public class JournalController : MonoBehaviour
 
     public Canvas canvas;
 
+    public Image page1;
+    public Image page2;
+    public Image page3;
+
+    private int currentPage = 1;
+
     void Update()
     {
         if (Input.GetKeyUp (KeyCode.Q)) {
             canvas.enabled = !canvas.enabled;
-        }	
+        }
+
+        if(Input.GetKeyUp (KeyCode.Tab) && canvas.enabled) {
+            NextPage();
+        }
+    }
+
+    void NextPage() {
+        if(currentPage < 3) { currentPage++; }
+        else { currentPage = 1; }
+
+        switch( currentPage ) {
+            case 1:
+                page3.enabled = false;
+                page1.enabled = true;
+                break;
+            case 2:
+                page1.enabled = false;
+                page2.enabled = true;
+                break;
+            case 3: 
+                page2.enabled = false;
+                page3.enabled = true;
+                break;
+        }
     }
 }
