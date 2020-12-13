94ffdb666964f19e4884cf949b82bb330e5e0503
Lily <lily.irvin@gmail.com>
2020-12-13 14:55:13 -0800

    Added one painting plaque

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -77,6 +77,7 @@ public class SelectionManager : MonoBehaviour
     }
 
     void TogglePlaque(string name) {
+        print(name);
         if (Input.GetMouseButtonDown(0)) {
             canvas.enabled = true;
             instructionLabel.GetComponent<TMP_Text>().text = "";
