b915cd1e7ba09659cebed031c7b8c44d7a182b6c
colinkirby <ckirby1@macalester.edu>
2020-12-15 13:38:58 -0600

    Intro scene in progress

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class SceneChanger : MonoBehaviour
 {
     // Start is called before the first frame update
 
     public Animator animator;
- 
+    public GameObject sceneSequenceManager;
 
     // Update is called once per frame
+    
     void Update()
     {
-        if(Input.GetMouseButtonDown(0)) {
-            FadeToScene();
+        if(Input.GetKeyDown(KeyCode.P)) {
+            sceneSequenceManager.GetComponent<SceneSequence>().Play();
         }
     }
 
---
+++ Assets/Scripts/SceneSequence.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSequence : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+
+    public Camera mainCamera;
+
+    public GameObject car;
+    public GameObject canvas1;
+    public GameObject canvas2;
+
+    public Transform pos;
+
+    private Animator cam1Anim;
+
+    private Animator cam2Anim;
+
+    public Animator carAnim;
+
+
+
+    void Start() {
+        cam1 = cam1.GetComponent<Camera>();
+        cam2 = cam2.GetComponent<Camera>();
+        mainCamera = mainCamera.GetComponent<Camera>();
+        pos = GetComponent<Transform>();
+
+        cam1Anim = pos.Find("SceneCamera1").GetComponent<Animator>();
+        cam2Anim = pos.Find("SceneCamera2").GetComponent<Animator>();
+        carAnim = car.GetComponent<Animator>();
+
+    }
+    public void Play()
+    {
+        canvas1.SetActive(false);
+
+
+        StartCoroutine ( Sequence());
+    }
+
+    // Update is called once per frame
+    IEnumerator Sequence() {
+        // yield return new WaitForSeconds(2);
+        canvas2.SetActive(false);
+        cam1.enabled = true;
+        mainCamera.enabled = false;
+        cam1Anim.Play("cam1");
+        carAnim.Play("car");
+        yield return new WaitForSeconds(8);
+        
+        cam2.enabled = true;
+        cam1.enabled = false;
+        cam2Anim.Play("cam2");
+        yield return new WaitForSeconds(8);
+        yield break;
+    }
+}
